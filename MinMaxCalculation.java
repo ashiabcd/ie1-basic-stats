06847b19623509c5498323501edafc5d37ae6d4c
Debarshiya Chandra <debarshiya@gmail.com>
2024-10-05 00:47:05 -0400

    Added comments in the MinMaxCalculation.java file

--- MinMaxCalculation.java
+++ MinMaxCalculation.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class MinMaxCalculation {
     public static void main(String[] args) {
+        // Input from the user
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter numbers separated by spaces:");
-
+        
+        // Read the input as string, assuming the user inputs the set of numbers with spaces inbetween the numbers as a string
         String input = scanner.nextLine();
         String[] numbers = input.split(" ");
 
-        // Initialize min and max with first number
+        // Initialize min and max with Integer Max and Integer Min
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
+        // Calculate the maximum and minimum value
         for (String number : numbers) {
             int num = Integer.parseInt(number);
             if (num < min) {
